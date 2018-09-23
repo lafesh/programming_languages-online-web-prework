511bf46aae784ca999b0f5acc682dbd64f8d2164
Gina Fedder <ginafedder@yahoo.com>
2018-09-23 00:29:01 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,6 @@ def reformat_languages(languages)
   languages.each do |style, lang|
     lang.each do |type, string|
       new_hash[type] ||= string
-      
       new_hash[type][:style] ||= []
       new_hash[type][:style] << style
     end
